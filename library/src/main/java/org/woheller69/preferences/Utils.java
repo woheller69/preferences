64c2173c103c24eb38f3941324d4f3806d74c3f1
woheller69 <wolfgang.heller@web.de>
2022-08-11 22:18:37 +0200

    change to getKey()

--- library/src/main/java/org/woheller69/preferences/Utils.java
+++ library/src/main/java/org/woheller69/preferences/Utils.java
@@ -8,7 +8,9 @@ import java.util.Base64;
 
 public class Utils {
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public static String apiKey(String key){
+    public static String getKey(String key){
+        StringBuilder sb = new StringBuilder(key);
+        key = sb.reverse().toString();
         Base64.Decoder dec = Base64.getDecoder();
         return new String(dec.decode(key));
     }
